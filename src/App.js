35fda996192125528990677742b78057e1df3a9f
Sahibdeep Singh <sahibdeepsinghj@gmail.com>
2024-07-24 21:20:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
-import Testimonials from "./Components/Testimonials";
-import Portfolio from "./Components/Portfolio";
 
 import "./App.css";
 
@@ -26,8 +24,8 @@ const App = () => {
       <Header data={resumeData.main} />
       <About data={resumeData.main} />
       <Resume data={resumeData.resume} />
-      <Portfolio data={resumeData.portfolio} />
-      <Testimonials data={resumeData.testimonials} />
+      {/* <Portfolio data={resumeData.portfolio} /> */}
+      {/* <Testimonials data={resumeData.testimonials} /> */}
       <Contact data={resumeData.main} />
       <Footer data={resumeData.main} />
     </div>
